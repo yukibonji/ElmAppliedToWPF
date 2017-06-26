19d553cc1ac9aa959b98121017a1089cc58bf5a2
Bertrand Cachet <bertrand.cachet@gmail.com>
2017-06-26 16:16:42 +0200

    REF: Split NumericUpDown component and Counters view in different modules

--- src/Counters/App.fs
+++ src/Counters/App.fs
@@ -4,61 +4,53 @@ open System
 
 
 
-/// The model bound to the view
-type UpDownModel =
-    { Value : double }
-
-type Model =
-    { UpDown1 : UpDownModel ; UpDown2 : UpDownModel }
-
-/// Different messages that can flow through the system.
-type UpDownMsg =
+module UpDown =
+    type Model =
+        { Value : double }
+       
+    type Msg =
     | Up
     | Down
 
-type Msg =
-    | UpDownMsg1 of UpDownMsg
-    | UpDownMsg2 of UpDownMsg
+    let init () = { Value = 0.0 }
 
-[<AutoOpen>]
-/// Contains code for event-driven subscriptions.
-module Subscriptions =
-    let none = null
-    
+    let update msg m =
+        match msg with
+        | Up    -> { m with Value = m.Value + 1.0 }
+        | Down  -> { m with Value = m.Value - 1.0 }
 
-/// Start state
-let initUpDown () = { Value = 0.0 }
-let init _ = { UpDown1 = initUpDown(); UpDown2 = initUpDown() }
-        
-/// The main Msg loop.
-let updateUpDown msg m =
-    match msg with
-    | Up    -> { m with Value = m.Value + 1.0 }
-    | Down  -> { m with Value = m.Value - 1.0 }
-   
-let update msg model =
-    match msg with
-    | UpDownMsg1 (m) -> { model with UpDown1 = (updateUpDown m model.UpDown1)}
-    | UpDownMsg2 (m) -> { model with UpDown2 = (updateUpDown m model.UpDown2)}
-
-
-/// Hooks up WPF bindings to the model.
-let viewUpDown _ _ =
-    [ "Value" |> Binding.oneWay (fun m -> m.Value)
-      "Up"    |> Binding.cmd (fun _ -> Up)
-      "Down"  |> Binding.cmd (fun _ -> Down) ]
-
-let view model _ =
-    let upDownViewBinding : ViewBindings<UpDownModel, UpDownMsg> = 
+    let view _ _ =
         [ "Value" |> Binding.oneWay (fun m -> m.Value)
           "Up"    |> Binding.cmd (fun _ -> Up)
           "Down"  |> Binding.cmd (fun _ -> Down) ]
-    ["UpDown1" |> Binding.vm (fun m -> m.UpDown1) upDownViewBinding (fun msg -> UpDownMsg1(msg))
-     "UpDown2" |> Binding.vm (fun m -> m.UpDown2) upDownViewBinding (fun msg -> UpDownMsg2(msg)) ]
+
+module Counters = 
+    
+    type Model =
+        { UpDown1 : UpDown.Model ; UpDown2 : UpDown.Model }
+
+    type Msg =
+        | UpDownMsg1 of UpDown.Msg
+        | UpDownMsg2 of UpDown.Msg
+
+    let init _ = { UpDown1 = UpDown.init(); UpDown2 = UpDown.init() }
+
+    let update msg model =
+        match msg with
+        | UpDownMsg1 (m) -> { model with UpDown1 = (UpDown.update m model.UpDown1)}
+        | UpDownMsg2 (m) -> { model with UpDown2 = (UpDown.update m model.UpDown2)}
+        
+    let view model _ =
+        let upDownViewBinding : ViewBindings<UpDown.Model, UpDown.Msg> = 
+            [ "Value" |> Binding.oneWay (fun m -> m.Value)
+              "Up"    |> Binding.cmd (fun _ -> UpDown.Up)
+              "Down"  |> Binding.cmd (fun _ -> UpDown.Down) ]
+        ["UpDown1" |> Binding.vm (fun m -> m.UpDown1) upDownViewBinding (fun msg -> UpDownMsg1(msg))
+         "UpDown2" |> Binding.vm (fun m -> m.UpDown2) upDownViewBinding (fun msg -> UpDownMsg2(msg)) ]
 
 [<STAThread; EntryPoint>]
 let main _ =
-    Program.mkSimple init update view
+    Program.mkSimple Counters.init Counters.update Counters.view
     |> Program.withSubscription (fun _ -> [ ] |> List.map Cmd.ofSub |> Cmd.batch)
     |> Program.withConsoleTrace
     |> Program.runWindow (Views.MainWindow())
