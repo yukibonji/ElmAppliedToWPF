11c63a551d16db63a50fc1e47a75921fdc4965f7
Bertrand Cachet <bertrand.cachet@gmail.com>
2017-07-17 14:02:11 +0200

    ADD(Counters): Display sum of all the UpDown Component inside the ListView

--- src/Counters/App.fs
+++ src/Counters/App.fs
@@ -1,8 +1,8 @@
+open System
+
 open Gjallarhorn
 open Gjallarhorn.Bindable
 open Gjallarhorn.Wpf
-open System
-
 
 
 module UpDown =
@@ -54,6 +54,7 @@ module Counters =
         | UpDownMsg (udmsg, guid) -> model |> List.map (fun m -> (if (fst m) = guid then guid, UpDown.update udmsg (snd m) else m))
     
     let viewBindings source (model : ISignal<Model>) =
+        model |> Signal.map ((List.map snd) >> List.map (fun m -> m.Value) >> (List.fold (+) 0.0)) |> Binding.toView source "Sum"
         [ 
           BindingCollection.toView source "Items" model (fun source2 tup -> (UpDown.viewBindings source2 (tup |> Signal.map snd)))
           |> Observable.map (fun (msg : UpDown.Msg, counter : Guid * UpDown.Model) -> UpDownMsg (msg, (fst counter)))
